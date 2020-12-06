6236b0a9385fc20e80110ec1a1578d67faf91b74
Fabio <fabio.sangregorio@gmail.com>
2020-12-06 11:52:23 +0100

    Restructure folders, add py39 whls, export functions

---
+++ notebooks/utils/filters.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from shapely.geometry import Point
+
+def filter_grid_on_shapefile(lats, lons, shp, plot_grid=False, plot_filter=False):
+    # create the latitudes/longitudes grid
+    coords = [(lat, lon) for lat in lats for lon in lons]
+
+    lats = [coord[0] for coord in coords]
+    lons = [coord[1] for coord in coords]
+
+    if plot_grid:
+        plt.figure()
+        plt.title("Observations Grid")
+        plt.xlabel("longitutde")
+        plt.ylabel("latitude")
+        plt.scatter(lons, lats, c="r")
+        plt.show()
+
+    # consider only the points inside the shape
+    grid = [
+        (round(lat, 2), round(lon, 2))
+        for lat, lon in zip(lats, lons)
+        if shp.geometry.contains(Point(lon, lat)).bool()
+    ]
+
+    if plot_filter:
+        lats = [coord[0] for coord in grid]
+        lons = [coord[1] for coord in grid]
+        plt.figure()
+        shp.plot()
+        plt.scatter(lons, lats, c="r")
+        plt.title("Filtered Observations Grid")
+        plt.xlabel("longitutde")
+        plt.ylabel("latitude")
+        plt.show()
+
+    return grid
+
+
+def filter_data_on_shapefile(data, lats, lons, shp, plot_grid=False, plot_filter=False):
+    lats = [round(lat, 2) for lat in lats.to_series().tolist()]
+    lons = [round(lon, 2) for lon in lons.to_series().tolist()]
+
+    result = np.ones((252, len(lats), len(lons)))
+    result.fill(np.nan)
+
+    grid = filter_grid_on_shapefile(
+        lats=lats, lons=lons, shp=shp, plot_grid=plot_grid, plot_filter=plot_filter
+    )
+
+    for lat_idx, lat in enumerate(lats):
+        for lon_idx, lon in enumerate(lons):
+
+            if (lat, lon) in grid:
+                for time_idx in range(len(data.time.to_series().tolist())):
+                    result[time_idx][lat_idx][lon_idx] = data[
+                        time_idx, lat_idx, lon_idx
+                    ].values
+    return result
---
+++ notebooks/utils/transformations.py
@@ -0,0 +1,69 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def adjust_var_grid(ds_ammoniaca, ds_temperature, var):
+    """
+    Adjust grid of input variable layer
+    adjust_var_grid("../datasets/CAMS-GLOB-ANT_Glb_0.1x0.1_anthro_nh3_v4.2_monthly_lombardia.nc", "../datasets/temperature.nc", "t2m")
+    """
+    ds_ammoniaca = xr.load_dataset(ds_ammoniaca)  # Copernicus (0.1°x0.1°)
+    lat_bounds = slice(44.74, 46.56)
+    lon_bounds = slice(8.5, 11.25)
+
+    ammoniaca = ds_ammoniaca.agl.sel(lat=lat_bounds, lon=lon_bounds)
+
+    ds_temperature = xr.load_dataset(ds_temperature)  # Copernicus (0.1°x0.1°)
+    lat_bounds = slice(46.66, 44.64)  # Invertiti
+    lon_bounds = slice(8.5, 11.35)
+
+    var = ds_temperature.t2m.sel(latitude=lat_bounds, longitude=lon_bounds)
+    lai_hv = ds_temperature.lai_hv.sel(latitude=lat_bounds, longitude=lon_bounds)
+    lai_lv = ds_temperature.lai_lv.sel(latitude=lat_bounds, longitude=lon_bounds)
+
+    var = var.transpose("time", "latitude", "longitude")
+    var = var.sortby("latitude", ascending=True)  # Riordina
+
+    lats = var.latitude
+    lons = var.longitude
+    ammoniaca_lats = ammoniaca.lat
+    ammoniaca_lons = ammoniaca.lon
+
+    coords = [
+        (lat, lon) for lat in lats.to_series() for lon in lons.to_series().tolist()
+    ]
+    ammoniaca_coords = [
+        (lat, lon)
+        for lat in ammoniaca_lats.to_series()
+        for lon in ammoniaca_lons.to_series().tolist()
+    ]
+
+    lats = [coord[0] for coord in coords]
+    lons = [coord[1] for coord in coords]
+
+    times = var.time.to_series().tolist()
+
+    t2m_adjusted_data = np.ones((len(times), len(ammoniaca.lat), len(ammoniaca.lon)))
+    t2m_adjusted_data.fill(np.nan)
+
+    for t in range(len(times)):
+        print("Adjusting time: " + str(t))
+        for lt in range(1, len(ammoniaca.lat) - 1):
+            for ln in range(1, len(ammoniaca.lon) - 1):
+                val = (
+                    var[t][lt - 1][ln - 1]
+                    + var[t][lt - 1][ln + 1]
+                    + var[t][lt + 1][ln - 1]
+                    + var[t][lt + 1][ln + 1]
+                )
+                t2m_adjusted_data[t][lt][ln] = val / 4
+
+    t2m_adjusted = xr.DataArray(
+        t2m_adjusted_data,
+        coords=[times, ammoniaca.lat, ammoniaca.lon],
+        dims=["time", "lat", "lon"],
+    )
+
+    print("Adjusted!")
+    return t2m_adjusted
