cdb8e0acfe8ace6f7b252be9cfa470cac518b090
Samuele Ferri <samuelexferri@gmail.com>
2020-12-17 12:02:29 +0100

    Add cross validation

---
+++ notebooks/utils/cross_validation.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import sklearn.linear_model as skl_lm
+import matplotlib.pyplot as plt
+
+
+def cross_validation(data, lon, lat, start_time, end_time, k):
+    """
+    Cross validation
+    """
+
+    # TODO
