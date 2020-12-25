7c33768237afe00a6c36ddef60be83ed4b635c3e
Fabio Sangregorio <fabio.sangregorio@gmail.com>
2020-12-25 10:09:40 +0100

    Add residuals analysis notebook

--- notebooks/utils/plots.py
+++ notebooks/utils/plots.py
@@ -11,3 +11,21 @@ def plot_shapefile(shapefile, epsg=4326, title="", xlabel="", ylabel=""):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_title(title)
+
+
+import numpy as np
+from scipy.stats import norm
+
+
+def plot_distributions(ts, title):
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+    result = axs[0].hist(ts, bins=20)
+    fig.suptitle(title)
+    mean = np.mean(ts)
+    sigma = np.sqrt(np.var(ts))
+    x = np.linspace(min(ts), max(ts), 100)
+    dx = result[1][1] - result[1][0]
+    scale = len(ts) * dx
+    axs[0].plot(x, norm.pdf(x, mean, sigma) * scale)
+    axs[1] = ts.plot(kind="kde")
+    plt.show()
